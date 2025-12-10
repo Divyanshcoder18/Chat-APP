bcc11dfa93deb8ac063a3167a2176b7d34a0be35
Divyanshcoder18 <kaliadivyansh77@gmail.com>
2025-12-10 13:39:26 +0530

    hi

--- backend/index.js
+++ backend/index.js
@@ -34,12 +34,12 @@ app.use("/api/user", userRouter);
 //app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
 //app.get(/.*/, (req, res) => {
- // res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 //});
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
 });
 
 
